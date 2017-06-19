b069d7c2f684f670b2865385721c4e242f9e1155
Aleksandr <alexff91@gmail.com>
2017-06-20 01:22:45 +0300

    Update app.py
    add static content support

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
 from flask import Flask
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 @app.route("/")
-def hello():
-    return "Hello World!"
-
-@app.route("/nikita")
-def helloNikita():
-    return "Hello Nikita!"
+def index():
+    return app.send_static_file('index.html')
